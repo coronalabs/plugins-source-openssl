1b52a57d8d7a0b6afb5fd91d0c56e8a448d6fbcc
Albert Yale <albert@coronalabs.com>
2014-01-27 13:52:21 -0800

    Update OpenSSL iOS build script from iphoneos6.1 to iphoneos7.0.

--- ios/build.sh
+++ ios/build.sh
@@ -35,10 +35,10 @@ echo "OUTPUT_DIR: $OUTPUT_DIR"
 xcodebuild -project "$path/Plugin.xcodeproj" -target $TARGET_NAME -configuration $CONFIG clean
 
 # Build iOS.
-xcodebuild -project "$path/Plugin.xcodeproj" -target $TARGET_NAME -configuration $CONFIG -sdk iphoneos6.1
+xcodebuild -project "$path/Plugin.xcodeproj" -target $TARGET_NAME -configuration $CONFIG -sdk iphoneos7.0
 
 # Build iOS-sim.
-xcodebuild -project "$path/Plugin.xcodeproj" -target $TARGET_NAME -configuration $CONFIG -sdk iphonesimulator6.1
+xcodebuild -project "$path/Plugin.xcodeproj" -target $TARGET_NAME -configuration $CONFIG -sdk iphonesimulator7.0
 
 # Create a universal binary.
 lipo -create "$path"/build/$CONFIG-iphoneos/lib$TARGET_NAME.$OUTPUT_SUFFIX "$path"/build/$CONFIG-iphonesimulator/lib$TARGET_NAME.$OUTPUT_SUFFIX -output "$OUTPUT_DIR"/lib$TARGET_NAME.$OUTPUT_SUFFIX
