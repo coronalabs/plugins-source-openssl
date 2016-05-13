80c42b9cd3a93cc81dd38a94b218d42dc0428a7e
Perry Clarke <perry@coronalabs.com>
2016-05-13 11:16:36 -0700

    Added tvOS port

---
+++ tvos/Corona_plugin_openssl.old/Corona_plugin_openssl/Corona_plugin_openssl.h
@@ -0,0 +1,19 @@
+//
+//  Corona_plugin_openssl.h
+//  Corona_plugin_openssl
+//
+//  Created by Perry Clarke on 5/11/16.
+//  Copyright © 2016 Corona Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Corona_plugin_openssl.
+FOUNDATION_EXPORT double Corona_plugin_opensslVersionNumber;
+
+//! Project version string for Corona_plugin_openssl.
+FOUNDATION_EXPORT const unsigned char Corona_plugin_opensslVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Corona_plugin_openssl/PublicHeader.h>
+
+
